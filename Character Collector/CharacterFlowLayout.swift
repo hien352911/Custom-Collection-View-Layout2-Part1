9da33a492cf3289fdccc4ce75316d2f92824407e
buiduyhien <hien.bd@seesaa.vn>
2020-06-28 10:20:43 +0700

    implement CharacterFlowLayout to scale item in centerY of screen

---
+++ Character Collector/CharacterFlowLayout.swift
@@ -0,0 +1,83 @@
+//
+//  CharacterFlowLayout.swift
+//  Character Collector
+//
+//  Created by buiduyhien on 6/28/20.
+//  Copyright © 2020 Razeware, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CharacterFlowLayout: UICollectionViewFlowLayout {
+    let standardItemAlpha: CGFloat = 0.5
+    let standardItemScale: CGFloat = 0.5
+    
+    override func prepare() {
+        super.prepare()
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let attributes = super.layoutAttributesForElements(in: rect)
+        var attributesCopy = [UICollectionViewLayoutAttributes]()
+        
+        for itemAttributes in attributes! {
+            let itemAttributesCopy = itemAttributes.copy() as! UICollectionViewLayoutAttributes
+            
+            changeLayoutAttributes(itemAttributesCopy)
+            attributesCopy.append(itemAttributesCopy)
+        }
+        
+        return attributesCopy
+    }
+    
+    func changeLayoutAttributes(_ attributes: UICollectionViewLayoutAttributes) {
+        let collectionCenter = collectionView!.frame.size.height / 2
+        let offset = collectionView!.contentOffset.y
+        /*
+         Vì itemSize set là (200, 200) mà màn hình iPhone 7, width là 375, nên chỉ đủ 1 item trong 1 row
+         Cơ chế mặc định của collection view là center nên nếu chỉ có 1 item trên 1 row thì nó sẽ ở giữa
+         Vậy center của item 0 là (375/2, 200/2)
+         375 là width của iPhone 7, 200 là height của cell
+         */
+        let normalizedCenter = attributes.center.y - offset
+        
+        /*
+         minimumLineSpacing default = 10
+         maxDistance const
+         */
+        let maxDistance = self.itemSize.height + self.minimumLineSpacing
+        /*
+         max of distance is maxDistance
+         distance = {0, maxDistance)
+         */
+        let distance = min(abs(collectionCenter - normalizedCenter), maxDistance)
+        
+        /*
+         ratio = {0, 1}
+         */
+        let ratio = (maxDistance - distance) / maxDistance
+        /*
+         ratio * (1 - self.standardItemAlpha) = {0, 0.5)
+         alpha = ratio * (1 - self.standardItemAlpha) + self.standardItemAlpha = {0.5, 1}
+         */
+        let alpha = ratio * (1 - self.standardItemAlpha) + self.standardItemAlpha
+        /*
+         scale = {0.5 ,1}
+         */
+        let scale = ratio * (1 - self.standardItemScale) + self.standardItemScale
+        
+        attributes.alpha = alpha
+        /*
+         Khi scale thì center giữ nguyên, width, height thay đổi trước rồi từ đó origin thay đổi
+         Before: (87.5, 0, 200, 200)
+         => center = (187.5, 100)
+         scale = 0.5
+         => new (width, height) = (100, 100)
+         CenterX = width / 2 + originX
+         => new originX = centerX - width / 2 = 187.5 - 100/2 = 137.5
+         CenterY = height / 2 + originY
+         => new originY = centerY - height / 2 = 100 - 100/2 = 50
+         */
+        attributes.transform3D = CATransform3DScale(CATransform3DIdentity, scale, scale, 1)
+    }
+}
